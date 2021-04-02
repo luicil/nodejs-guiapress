0d85beab7a563305a6ab8670069f68ccd8c5b3a8
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-02 11:04:33 -0300

    arquitetura

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const port = 80;
 const conn = require("./database/database");
+const categoriesController = require("./categories/controller/CategoriesController");
+const articlesController = require("./atricles/controller/ArticlesController");
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 conn
     .authenticate()
