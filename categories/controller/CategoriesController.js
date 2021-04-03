b54c3ae741e04900d34afeab6e757a5b1dee8b17
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-03 12:21:05 -0300

    Cadastro de Categorias

--- categories/controller/CategoriesController.js
+++ categories/controller/CategoriesController.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../model/Category");
+const slugify = require("slugify");
 
 router.get("/categories", (req, res) =>{
     res.send("Rota de categorais");
@@ -7,6 +9,21 @@ router.get("/categories", (req, res) =>{
 
 router.get("/admin/categories/new", (req, res) =>{
     res.render("admin/categories/new");
+
+});
+
+router.post("/categories/save", (req, res) =>{
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() =>{
+            res.redirect("/");
+        })
+    } else {
+        res.redirect("/admin/categories/new")
+    }
 
 });
 
