4abec1ea8fd47dc4977df8e404c795e45c87a537
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-03 18:58:48 -0300

    Tabela

--- categories/controller/CategoriesController.js
+++ categories/controller/CategoriesController.js
@@ -12,6 +12,10 @@ router.get("/admin/categories/new", (req, res) =>{
 
 });
 
+router.get("/admin/categories", (req, res) =>{
+    res.render("admin/categories/index");
+});
+
 router.post("/categories/save", (req, res) =>{
     var title = req.body.title;
     if(title != undefined){
@@ -19,7 +23,7 @@ router.post("/categories/save", (req, res) =>{
             title: title,
             slug: slugify(title)
         }).then(() =>{
-            res.redirect("/");
+            res.redirect("admin/categories/index");
         })
     } else {
         res.redirect("/admin/categories/new")
